f7fe86a0cd12ac9304cb03013eb4987ab49b6abe
oobol2000oo <boly456@gmail.com>
2016-10-29 07:22:44 +0700

    Add DEMSO0 in NTUCoder

---
+++ NTUCoder/DEMSO0.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+int n, ans;
+
+int main(){
+    scanf("%d", &n);
+    for (int i = 2; i <= n; ++i){
+        int temp = i;
+        while (temp % 5 == 0){
+            ++ans;
+            temp = temp / 5;
+        }
+    }
+    printf("%d", ans);
+    return 0;
+}
