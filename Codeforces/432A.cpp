f61c201dabb665512a9d1f485db448fed497786e
oobol2000oo <boly456@gmail.com>
2016-11-08 11:30:35 +0700

    Optimization 432A in Codeforces

--- Codeforces/432A.cpp
+++ Codeforces/432A.cpp
@@ -1,20 +1,14 @@
 #include <bits/stdc++.h>
 
-int n, k, *a, ans;
+int n, k, ans;
 
 int main(){
-    int temp = 0;
     scanf("%d%d", &n, &k);
-    a = new int [n];
     for (int i = 0; i < n; ++i){
-        scanf("%d", &a[i]);
-        if (a[i] + k <= 5) ++temp;
-        if (temp == 3){
-            temp = 0;
-            ++ans;
-        }
+        int temp;
+        scanf("%d", &temp);
+        if (temp + k <= 5) ++ans;
     }
-    printf("%d", ans);
-    delete [] a;
+    printf("%d", ans / 3);
     return 0;
 }
