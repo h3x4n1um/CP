afb5dffdde9531246dacb4558748f15aefb6cdeb
oobol2000oo <boly456@gmail.com>
2016-12-02 21:34:21 +0700

    Add NKFLOW in SPOJ

---
+++ SPOJ/NKFLOW.cpp
@@ -0,0 +1,84 @@
+#define oo 1000000007
+#define maxN 1007
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m, s, t, ans;
+int a[maxN][maxN], f[maxN][maxN], trace[maxN], delta[maxN];
+
+bool Find_Path(){
+    queue <int> q;
+
+    for (int i = 1; i <= n; ++i){
+        trace[i] = 0;
+    }
+    delta[s] = oo;
+    trace[s] = s;
+    q.push(s);
+
+    while(!q.empty()){
+        int u = q.front();
+
+        q.pop();
+        for (int v = 1; v <= n; ++v){
+            if (trace[v] == 0){
+                if (a[u][v] - f[u][v] > 0){
+                    trace[v] = u;
+                    delta[v] = min(delta[u], a[u][v] - f[u][v]);
+                    q.push(v);
+                }
+                else if (f[v][u] > 0){
+                    trace[v] = -u;
+                    delta[v] = min(delta[u], f[v][u]);
+                    q.push(v);
+                }
+
+                if (trace[v] != 0 && v == t) return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+void Inc_Flow(){
+    int v = t;
+    while (v != s){
+        int u = trace[v];
+
+        if (u > 0) f[u][v] += delta[t];
+        else{
+            u = -u;
+            f[v][u] -= delta[t];
+        }
+        v = u;
+    }
+}
+
+void Ford_Fulkerson(){
+    while (Find_Path()) Inc_Flow();
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("NKFLOW.inp", "r", stdin);
+    #endif // ONLINE_JUDGE
+
+    scanf("%d%d%d%d", &n, &m, &s, &t);
+    for (int i = 0; i < m; ++i){
+        int u, v, w;
+
+        scanf("%d%d%d", &u, &v, &w);
+        a[u][v] = w;
+    }
+
+    Ford_Fulkerson();
+
+    for (int i = 1; i <= n; ++i){
+        ans += f[s][i];
+    }
+    printf("%d", ans);
+    return 0;
+}
